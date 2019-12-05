83fb92cacf5db36d32e561b6aaf74418f4cda98b
Šimon Trusina <simon.trusina@gmail.com>
2019-12-05 19:27:44 +0100

    specifikace kodovani pro nasazeni z RStudia

--- deploy.R
+++ deploy.R
@@ -1,2 +1,5 @@
 # deploy app
+tmp.enc <- options()$encoding
+options(encoding = "UTF-8")
 rsconnect::deployApp(appFileManifest = "data/fileManifest.txt", appName = "ukony")
+options(encoding = tmp.enc)
